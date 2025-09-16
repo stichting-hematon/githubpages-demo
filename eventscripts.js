f5182d7e42950e04d9cd04f3d86cd527995c041b
stichting-hematon <webmaster@hematon.nl>
2025-09-16 14:08:25 +0200

    Update eventscripts.js

--- eventscripts.js
+++ eventscripts.js
@@ -4,20 +4,8 @@ const goButton = document.getElementById("goButton");
 var formurl = "https://forms.office.com/Pages/ResponsePage.aspx?id=cZoyhOXi-Uifid8K47hDBuJ5oMSX115JtVjuBC6Gzx9UOVNNVlc1V0c2UDJQRzRJRTRISlRNNkgwMSQlQCN0PWcu&eventkey=234"
 
 function redirect() {
-    const overlay = document.querySelector(".overlay");
-    if (overlay) {
-        // Fade-out effect
-        overlay.style.transition = "opacity 1s";
-        overlay.style.opacity = 0;
-
-        // Redirect zodra fade-out klaar is
-        overlay.addEventListener("transitionend", () => {
-            window.location.href = formurl;
-        }, { once: true });
-    } else {
-        // Fallback: direct redirect
-        window.location.href = formurl;
-    }
+    // Direct doorsturen zonder fade
+    window.location.href = https://forms.office.com/Pages/ResponsePage.aspx?id=cZoyhOXi-Uifid8K47hDBuJ5oMSX115JtVjuBC6Gzx9UOVNNVlc1V0c2UDJQRzRJRTRISlRNNkgwMSQlQCN0PWcu&eventkey=234";
 }
 
 // Countdown automatisch
@@ -35,5 +23,3 @@ goButton.addEventListener("click", () => {
     clearInterval(interval);
     redirect();
 });
-
-
